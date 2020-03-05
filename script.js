5ab345837a752b97f74505d6a1c321816e9fe172
Chris Blaylock <52610450+blaylockc09@users.noreply.github.com>
2020-03-05 14:03:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+/* Hands-on-Project 9-2
+Author: Chris Blaylock 
+Date: 03/05/2020
+filename: script.js */
+
+"use strict"
+
+populateInfo() {
+    if (document.cookie) {
+        var uname = document.cookie;
+        uname = uname.substring(uname.lastIndexOf("=") + 1);
+        document.getElementById("usernameinput").value = uname;
+    }
+}
+
+handleSubmit(evt) {
+    if (evt.preventDefault) {
+        evt.preventDefault();
+    } else {
+        evt.returnValue = false;
+    }
+    processCookie();
+    document.getElementsByTagName("form")[0].submit();
+}
+
+function createEventListener() {
+    var loginForm = document.getElementsByTagName("form")[0];
+    if (loginForm.addEventListener) {
+        loginForm.addEventListener("submit", handleSubmit, false);
+    } else if (loginForm.attachEvent) {
+        loginForm.attachEvent("onsubmit", handleSubmit);
+    }
+}
+
+function setUpPage() {
+    populateInfo();
+    createEventListener();
+}
+
+if (window.addEventListener) {
+    window.addEventListener("load", setUpPage, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", setUpPage);
+}
